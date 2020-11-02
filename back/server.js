8a0eb232a92181fea39a9dc6d56538c9383a2482
BaptisteMahe <mahe.baptiste.19@gmail.com>
2020-11-02 17:37:39 +0100

    Add first version of frontEnd

--- back/server.js
+++ back/server.js
@@ -8,7 +8,7 @@ const PORT = 3000;
 var app = express();
 var files = fs.readdirSync('./drive');
 
-app.get('/', (req, res) => {
+app.get('/test', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
   res.write('<input type="file" name="filetoupload"><br>');
@@ -19,16 +19,16 @@ app.get('/', (req, res) => {
 
 app.post('/fileupload', (req, res) => {
   var form = new formidable.IncomingForm();
-    form.parse(req, function (err, fields, files) {
-      var oldpath = files.filetoupload.path;
-      var newpath = './drive/' + files.filetoupload.name;
-      console.log(files);
-      fs.copyFile(oldpath, newpath, function (err) {
-        if (err) throw err;
-        res.send('File uploaded and moved!');
-        res.end();
-      });
+  form.parse(req, function (err, fields, files) {
+    var oldpath = files.filetoupload.path;
+    var newpath = './drive/' + files.filetoupload.name;
+    // console.log(files);
+    fs.copyFile(oldpath, newpath, function (err) {
+      if (err) throw err;
+      res.send('File uploaded and moved!');
+      res.end();
     });
+  });
 });
 
 app.listen(PORT, () => {
--- front/src/App.vue
+++ front/src/App.vue
@@ -1,17 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="drive-app">
+    <img alt="Logo" src="./assets/logo.png" />
+    <Upload />
+    <div class="file-list" v-if="fileArray.length > 0">
+      <File v-for="file in fileArray" :key="file" v-bind:fileName="file" />
+    </div>
+    <div v-else class="empty-file-list">There is no files in your drive</div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import File from "./components/File";
+import Upload from "./components/Upload";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Upload,
+    File,
+  },
+  data() {
+    return {
+      fileArray: [
+        "doc.txt",
+        "lol.pdf",
+        "attention.exe",
+        "object.json",
+        "helloWorld.py",
+      ],
+    };
+  },
+};
 </script>
 
 <style>
@@ -22,5 +41,17 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.file-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+
+.empty-file-list {
+  color: red;
+  font-size: 1.5em;
+  font-weight: bold;
 }
 </style>
