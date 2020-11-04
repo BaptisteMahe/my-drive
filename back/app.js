70f010a16d3fcbd840a88b8234214f6ec56bcc4a
BaptisteMahe <mahe.baptiste.19@gmail.com>
2020-11-04 15:07:54 +0100

    Add graphql for backend and uninstall it for frontend

--- back/app.js
+++ back/app.js
@@ -50,15 +50,6 @@ app.post('/fileUpload', (req, res) => {
   });
 });
 
-app.get('/test', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-  res.write('<input type="file" name="filetoupload"><br>');
-  res.write('<input type="submit">');
-  res.write('</form>');
-  return res.end();
-});
-
 /*--------<GraphQL>--------*/
 let schema = buildSchema(`
   type Query {
